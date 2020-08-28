e2eabb95617335df9c589d463b4c164fae586d9a
Jacek Jarczok <kson.eu@gmail.com>
2020-08-28 12:40:33 +0200

    Introduce Logo component

---
+++ src/Logo.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './Logo.css';
+
+class Logo extends Component {
+  render() {
+    return(
+      <div className="Logo">
+        <p>what to play ?</p>
+      </div>
+    );
+  }
+}
+
+export default Logo;
