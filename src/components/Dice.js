22966d5e656b4b7d097368bf8b761e704c6e3d61
Jacek Jarczok <kson.eu@gmail.com>
2020-09-17 10:15:48 +0200

    Develop Dice

---
+++ src/components/Dice.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import './Dice.css';
+
+class Dice extends Component {
+  constructor(props) {
+    super();
+  }
+  render() {
+    return(
+      <div class="Dice-container">
+        <div class="Dice">
+          <div class="Dice-side front">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div class="Dice-side back">
+            <span></span>
+            <span></span>
+          </div>
+          <div class="Dice-side left">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div class="Dice-side right">
+            <span></span>
+          </div>
+          <div class="Dice-side top">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div class="Dice-side bottom">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Dice;
--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './Logo.css';
+import Dice from './Dice';
 
 class Logo extends Component {
   constructor(props) {
@@ -9,6 +10,7 @@ class Logo extends Component {
     return(
       <div className="Logo">
         <h1 title="what to play ?">what to play ?</h1>
+        <Dice />
       </div>
     );
   }
