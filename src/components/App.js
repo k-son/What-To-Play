4edb4bf8039d3778dae5d6fb0f3cec5c050ed28c
Jacek Jarczok <kson.eu@gmail.com>
2020-10-14 15:13:01 +0200

    Move cookie consent to the bottom

--- src/components/App.js
+++ src/components/App.js
@@ -447,10 +447,9 @@ class App extends Component {
           </ul>
         </div>
 
-        {/* Cookie consent. Out of the normal document flow. */}
+        {/* Cookie consent. Out of the normal document flow. [react-cookie-consent library] */}
         <CookieConsent
-          location={'none'}
-          style={{bottom: "80px", alignItems: "center", justifyContent: "flex-start", padding: "20px", backgroundColor: "rgba(0,0,0,0.75)", fontWeight: "300", lineHeight: "1.6", letterSpacing: "1px"}}
+          style={{alignItems: "center", justifyContent: "flex-start", padding: "20px", backgroundColor: "rgba(0,0,0,0.75)", fontWeight: "300", lineHeight: "1.6", letterSpacing: "1px"}}
           contentStyle={{flex: "0 1 auto", marginRight: "20px", marginBottom: "20px"}}
           buttonStyle={{height: "40px", borderRadius: "3px", fontSize: "16px"}}
           >This website uses cookies to enhance the user experience.
