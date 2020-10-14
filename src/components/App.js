1fa091a0fe4f9f77784b5b216ce858a83896c796
Jacek Jarczok <kson.eu@gmail.com>
2020-10-14 13:25:48 +0200

    Move cookie consent accept button to the left Tweak margins and padding

--- src/components/App.js
+++ src/components/App.js
@@ -450,7 +450,8 @@ class App extends Component {
         {/* Cookie consent. Out of the normal document flow. */}
         <CookieConsent
           location={'none'}
-          style={{bottom: "80px", alignItems: "center", padding: "0 20px", backgroundColor: "rgba(0,0,0,0.75)", fontWeight: "300", letterSpacing: "1px"}}
+          style={{bottom: "80px", alignItems: "center", justifyContent: "flex-start", padding: "20px", backgroundColor: "rgba(0,0,0,0.75)", fontWeight: "300", lineHeight: "1.6", letterSpacing: "1px"}}
+          contentStyle={{flex: "0 1 auto", marginRight: "20px", marginBottom: "20px"}}
           buttonStyle={{height: "40px", borderRadius: "3px", fontSize: "16px"}}
           >This website uses cookies to enhance the user experience.
         </CookieConsent>
