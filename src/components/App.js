1b11967d097a0fbcadfc3a32a228cba04b2c7379
Jacek Jarczok <kson.eu@gmail.com>
2020-11-04 10:08:22 +0100

    Remove comment

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,6 @@ import {ReactComponent as IconBackArrow} from '../icons/back-arrow.svg';
 import {ReactComponent as IconChoice} from '../icons/one.svg';
 import {ReactComponent as IconDices} from '../icons/dices.svg';
 import {ReactComponent as IconRefresh} from '../icons/refresh.svg';
-// Functionals
 import KeyDownListener from './KeyDownListener';
 import MouseDownListener from './MouseDownListener';
 // Styles
