636cd3f6e1c644023678f1024499fdafa39480bf
Jacek Jarczok <kson.eu@gmail.com>
2020-11-04 10:00:14 +0100

    Convert to functional components

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,30 +1,29 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './ProgressBar.css';
 import SongsLeft from './SongsLeft';
 
-class ProgressBar extends Component {
-  constructor(props) {
-    super();
-  }
-  render() {
-    let barCol;
-    if (this.props.progress > 66) {
+const ProgressBar = ({ progress, songsLeft }) => {
+  let barCol;
+    if (progress > 66) {
       barCol = 'var(--btn-color-yellow)'
-    } else if (this.props.progress > 33 && this.props.progress <= 66) {
+    } else if (progress > 33 && progress <= 66) {
       barCol = 'var(--btn-color-orange)'
     } else {
       barCol = 'var(--btn-color-red)'
     }
+
     return(
       <div className="ProgressBar">
-        <SongsLeft songsLeft={this.props.songsLeft} color={barCol} />
+        <SongsLeft 
+          songsLeft={songsLeft} 
+          color={barCol} 
+        />
         <div className="ProgressBar-bar">
-          <div style={{width: this.props.progress + '%', backgroundColor: barCol}}></div>
-          <span className="ProgressBar-count" style={{left: this.props.progress + '%', color: barCol}}>{this.props.progress}%</span>
+          <div style={{width: progress + '%', backgroundColor: barCol}}></div>
+          <span className="ProgressBar-count" style={{left: progress + '%', color: barCol}}>{progress}%</span>
         </div>
       </div>
     );
-  }
 }
 
 export default ProgressBar;
--- src/components/ProgressRing.js
+++ src/components/ProgressRing.js
@@ -1,72 +1,68 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './ProgressRing.css';
 
-class ProgressRing extends Component {
-  constructor(props) {
-    super();
+const ProgressRing = ({ sqSize, strokeWidth, percentage, songsLeft }) => {
+  
+  // Size of the enclosing square
+  const size = sqSize;
+  // SVG centers the stroke width on the radius, subtract out so circle fits in square
+  const radius = (sqSize - strokeWidth) / 2;
+  // Enclose cicle in a circumscribing square
+  const viewBox = `0 0 ${size} ${size}`;
+  // Arc length at 100% coverage is the circle circumference
+  const dashArray = radius * Math.PI * 2;
+  // Scale 100% coverage overlay with the actual percent
+  const dashOffset = dashArray - dashArray * percentage / 100;
+  // Stroke color 
+  let barCol;
+  if (percentage > 66) {
+    barCol = 'var(--btn-color-yellow)'
+  } else if (percentage > 33 && percentage <= 66) {
+    barCol = 'var(--btn-color-orange)'
+  } else {
+    barCol = 'var(--btn-color-red)'
   }
-  render() {
-    // Size of the enclosing square
-    const sqSize = this.props.sqSize;
-    // SVG centers the stroke width on the radius, subtract out so circle fits in square
-    const radius = (this.props.sqSize - this.props.strokeWidth) / 2;
-    // Enclose cicle in a circumscribing square
-    const viewBox = `0 0 ${sqSize} ${sqSize}`;
-    // Arc length at 100% coverage is the circle circumference
-    const dashArray = radius * Math.PI * 2;
-    // Scale 100% coverage overlay with the actual percent
-    const dashOffset = dashArray - dashArray * this.props.percentage / 100;
-    // Stroke color 
-    let barCol;
-    if (this.props.percentage > 66) {
-      barCol = 'var(--btn-color-yellow)'
-    } else if (this.props.percentage > 33 && this.props.percentage <= 66) {
-      barCol = 'var(--btn-color-orange)'
-    } else {
-      barCol = 'var(--btn-color-red)'
-    }
 
-    return (
-      <figure className="ProgressRing">
-        <svg
-            width={this.props.sqSize}
-            height={this.props.sqSize}
-            viewBox={viewBox}>
-            <circle
-              className="ProgressRing-backgroundCircle"
-              cx={this.props.sqSize / 2}
-              cy={this.props.sqSize / 2}
-              r={radius}
-              strokeWidth={`${this.props.strokeWidth}px`} 
-              style={{
-                fill: "none"
-              }}
-              />
-            <circle
-              className="ProgressRing-progressCircle"
-              cx={this.props.sqSize / 2}
-              cy={this.props.sqSize / 2}
-              r={radius}
-              strokeWidth={`${this.props.strokeWidth}px`}
-              // Start progress marker at 12 O'Clock
-              transform={`rotate(-90 ${this.props.sqSize / 2} ${this.props.sqSize / 2})`}
-              style={{
-                fill: "none",
-                strokeDasharray: dashArray,
-                strokeDashoffset: dashOffset,
-                stroke: barCol,
-                strokeLinecap: "round",
-                strokeLinejoin: "round"
-              }} />
-        </svg>
-        <div className="ProgressRing-text">
-          <p style={{color: barCol}}>{this.props.songsLeft}</p>
-          <p style={{color: barCol}}>({this.props.percentage}%)</p>
-          <p>songs left</p>
-        </div>
-      </figure>
-    );
-  }
+  return (
+    <figure className="ProgressRing">
+      <svg
+          width={sqSize}
+          height={sqSize}
+          viewBox={viewBox}>
+          <circle
+            className="ProgressRing-backgroundCircle"
+            cx={sqSize / 2}
+            cy={sqSize / 2}
+            r={radius}
+            strokeWidth={`${strokeWidth}px`} 
+            style={{
+              fill: "none"
+            }}
+            />
+          <circle
+            className="ProgressRing-progressCircle"
+            cx={sqSize / 2}
+            cy={sqSize / 2}
+            r={radius}
+            strokeWidth={`${strokeWidth}px`}
+            // Start progress marker at 12 O'Clock
+            transform={`rotate(-90 ${sqSize / 2} ${sqSize / 2})`}
+            style={{
+              fill: "none",
+              strokeDasharray: dashArray,
+              strokeDashoffset: dashOffset,
+              stroke: barCol,
+              strokeLinecap: "round",
+              strokeLinejoin: "round"
+            }} />
+      </svg>
+      <div className="ProgressRing-text">
+        <p style={{color: barCol}}>{songsLeft}</p>
+        <p style={{color: barCol}}>({percentage}%)</p>
+        <p>songs left</p>
+      </div>
+    </figure>
+  );
 }
 
 export default ProgressRing;
