f3a3182242d07f1d81de6b6f7296d115b0524203
Jacek Jarczok <kson.eu@gmail.com>
2020-11-04 10:07:29 +0100

    Convert to functional component

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,69 +1,63 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Button from './Button';
 import {ReactComponent as IconRefresh} from '../icons/refresh.svg';
 import {ReactComponent as IconClose} from '../icons/close.svg';
 import './Modal.css';
 
-
-class Modal extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div className={`Modal-${this.props.modal} ${this.props.confirmDialog === 'open' && 'padding-right-20'}`}>
-        <div className="Modal-buttons">
-          {this.props.progress < 100 && 
-            <Button 
-              addClassName="btn-reload" 
-              onClick={this.props.btnReload} 
-              icon={<IconRefresh />} 
-              title="Reload full setlist" 
-              ariaLabel="Reaload full setlist" 
-              tabIndex={this.props.confirmDialog === 'open' ? -1 : 0} 
-            />
-          }
+const Modal = ({ modal, confirmDialog, progress, btnReload, btnClose, songs, chooseSong, removeSong }) => {
+  return (
+    <div className={`Modal-${modal} ${confirmDialog === 'open' && 'padding-right-20'}`}>
+      <div className="Modal-buttons">
+        {progress < 100 && 
           <Button 
-            addClassName="btn-close" 
-            onClick={this.props.btnClose} 
-            icon={<IconClose />} 
-            title="Close choice view" 
-            ariaLabel="Close choice view" 
-            tabIndex={this.props.confirmDialog === 'open' ? -1 : 0} 
+            addClassName="btn-reload" 
+            onClick={btnReload} 
+            icon={<IconRefresh />} 
+            title="Reload full setlist" 
+            ariaLabel="Reaload full setlist" 
+            tabIndex={confirmDialog === 'open' ? -1 : 0} 
           />
-        </div>
-        <ul className="Modal-list">
-          {this.props.songs
-            .sort((a, b) => a > b ? 1 : -1)
-            .map(item => 
-              <li key={item}>
-                <button 
-                  className="ModalList-chooseBtn" 
-                  type="button" 
-                  onClick={this.props.chooseSong} 
-                  aria-label={item} 
-                  tabIndex={this.props.confirmDialog === 'open' ? -1 : 0}
-                >
-                  <div className="ModalList-circle">
-                    <div></div>
-                  </div>
-                  <p>{item}</p>
-                </button>
-                <button 
-                  className="ModalList-removeSongBtn" 
-                  type="button" 
-                  onClick={this.props.removeSong} 
-                  data-song={item} 
-                  title="Remove song from current list" 
-                  aria-label={`Remove '${item}' from current list`} 
-                  tabIndex={this.props.confirmDialog === 'open' ? -1 : 0}
-                ></button>
-              </li>
-          )}
-        </ul>
+        }
+        <Button 
+          addClassName="btn-close" 
+          onClick={btnClose} 
+          icon={<IconClose />} 
+          title="Close choice view" 
+          ariaLabel="Close choice view" 
+          tabIndex={confirmDialog === 'open' ? -1 : 0} 
+        />
       </div>
-    );
-  }
+      <ul className="Modal-list">
+        {songs
+          .sort((a, b) => a > b ? 1 : -1)
+          .map(item => 
+            <li key={item}>
+              <button 
+                className="ModalList-chooseBtn" 
+                type="button" 
+                onClick={chooseSong} 
+                aria-label={item} 
+                tabIndex={confirmDialog === 'open' ? -1 : 0}
+              >
+                <div className="ModalList-circle">
+                  <div></div>
+                </div>
+                <p>{item}</p>
+              </button>
+              <button 
+                className="ModalList-removeSongBtn" 
+                type="button" 
+                onClick={removeSong} 
+                data-song={item} 
+                title="Remove song from current list" 
+                aria-label={`Remove '${item}' from current list`} 
+                tabIndex={confirmDialog === 'open' ? -1 : 0}
+              ></button>
+            </li>
+        )}
+      </ul>
+    </div>
+  );
 }
 
 export default Modal;
