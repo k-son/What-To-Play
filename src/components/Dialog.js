4d412685187a04585e98b1cd5ac69a127a89cab2
Jacek Jarczok <kson.eu@gmail.com>
2020-11-04 10:03:20 +0100

    Convert to functional component

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -1,26 +1,21 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './Dialog.css';
 
-class Dialog extends Component {
-  constructor(props) {
-    super()
-  }
-  render() {
-    return(
-      <div className={`Dialog-${this.props.isOpen}`}>
-        <div className="Dialog-conent">
-          <div className="Dialog-text">
-            <p className="Dialog-question">{this.props.question}</p>
-            <p className="Dialog-songTitle">{this.props.songTitle !== ' ' && `'${this.props.songTitle}'`}</p>
-          </div>
-          <div className="Dialog-buttons">
-            <button className="Dialog-button Dialog-cancel" type="button" onClick={this.props.onCancel}>Cancel</button>
-            <button className="Dialog-button Dialog-ok" type="button" onClick={this.props.onConfirm}>OK</button>
-          </div>
+const Dialog = ({ isOpen, question, songTitle, onCancel, onConfirm }) => {
+  return(
+    <div className={`Dialog-${isOpen}`}>
+      <div className="Dialog-conent">
+        <div className="Dialog-text">
+          <p className="Dialog-question">{question}</p>
+          <p className="Dialog-songTitle">{songTitle !== ' ' && `'${songTitle}'`}</p>
+        </div>
+        <div className="Dialog-buttons">
+          <button className="Dialog-button Dialog-cancel" type="button" onClick={onCancel}>Cancel</button>
+          <button className="Dialog-button Dialog-ok" type="button" onClick={onConfirm}>OK</button>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Dialog;
