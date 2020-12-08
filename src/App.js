7ead15259d02f33252e25ff425736c85b9c3d5ce
Jacek Jarczok <kson.eu@gmail.com>
2020-12-08 13:27:22 +0100

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Switch, Link, Route } from 'react-router-dom';
 import Draw from './components/Draw';
 
 const App = () => {
   return (
     <div className="App">
-      <Draw />
+      <Switch>
+        <Route exact path="/">
+          <Draw />
+        </Route>
+        <Route exact path="/choice">
+        </Route>
+      </Switch>
     </div>
   );
 } 
