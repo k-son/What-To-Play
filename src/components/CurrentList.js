6a7809f8d355a92b70609e8551e7fac48a0039eb
Jacek Jarczok <kson.eu@gmail.com>
2020-12-23 14:49:52 +0100

    Refactor using styled-components

--- src/components/CurrentList.js
+++ src/components/CurrentList.js
@@ -1,16 +1,83 @@
 import React from 'react';
-import './CurrentList.css';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  max-width: 640px;
+  height: 136px;
+  margin: 0 auto;
+  margin-bottom: 40px;
+
+  @media screen (min-width: 1200px) {
+    width: 75%;
+    max-width: 1200px;
+  }
+
+  @media screen (min-height: 1000px) {
+    height: 180px;
+  }
+`;
+
+const List = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  max-height: 100%;
+  margin: 0 auto;
+  padding: 4px 16px;
+  list-style-type: none;
+  overflow-y: auto;
+`;
+
+const Item = styled.li`
+  display: inline;
+  position: relative;
+  font-size: calc(14px + 6 * ((100vw - 320px) / 1020));
+  font-weight: 300;
+  line-height: 1.4;
+  text-transform: capitalize;
+  text-align: center;
+  color: #999;
+
+  @media screen (min-width: 1000px) {
+    font-size: 18px;
+    line-height: 1.6;
+  }
+
+    &:not(:last-of-type) {
+      margin-right: 6px;
+    }
+
+    /* first Dot */
+    &:first-of-type > span {
+      display: none;
+    }
+`;
+
+const Dot = styled.span`
+  margin-right: 6px;
+  color: #666;
+`;
 
 function CurrentList({ songs }) {
   return (
-    <div className="CurrentList">
-      <ul className="CurrentList-list">
+    <Wrapper>
+      <List>
         {songs
           .sort((a, b) => a > b ? 1 : -1)
-          .map(item => <li key={item}><span className="CurrentList-dot">&bull;</span>{item}</li>
+          .map(item => (
+            <Item key={item}>
+              <Dot>&bull;</Dot>
+              {item}
+            </Item>
+          )
         )}
-      </ul>
-    </div>
+      </List>
+    </Wrapper>
   )
 };
 
