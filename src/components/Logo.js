647317f5f8ac42e643cd980350995a2434e248c1
Jacek Jarczok <kson.eu@gmail.com>
2020-12-23 14:22:38 +0100

    Refactor using styled-components

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,11 +1,77 @@
 import React from 'react';
-import './Logo.css';
+import styled from "styled-components";
 import Dice from './Dice';
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: ${({ theme }) => theme.logoHeight};
+  padding: 16px;
+`;
+
+const Logotype = styled.h1`
+  position: relative;
+  font-size: 24px;
+  font-weight: 900;
+  text-transform: uppercase;
+  text-align: center;
+  color: #777;
+  text-shadow: -1px 0px 0 #222;
+  margin-right: 16px;
+  transform: translateX(7px);
+
+    span {
+    position: relative;
+    display: inline-block;
+    letter-spacing: -2px;
+
+      &:nth-of-type(even) {
+        color: #999;
+        font-size: 24px;
+      }
+
+      /* t */
+      &:nth-of-type(4) {
+        transform: translateX(-1px);
+      }
+
+      /* t */
+      &:nth-of-type(5) {
+        transform: translateX(-3px);
+      }
+
+      /* o */
+      &:nth-of-type(6) {
+        transform: translateX(-3px);
+      }
+
+      /* p */
+      &:nth-of-type(7) {
+        transform: translateX(-3px);
+      }
+
+      /* l */
+      &:nth-of-type(8) {
+        transform: translateX(-4px);
+      }
+
+      /* a */
+      &:nth-of-type(9) {
+        transform: translateX(-5px);
+      }
+
+      /* y */
+      &:nth-of-type(10) {
+        transform: translateX(-7px);
+      }
+    }
+`;
 
 function Logo() {
   return (
-    <div className="Logo">
-      <h1 title="what to play ?">
+    <Wrapper>
+      <Logotype title="what to play ?">
         <span>w</span>
         <span>h</span>
         <span>a</span>
@@ -16,9 +82,9 @@ function Logo() {
         <span>l</span>
         <span>a</span>
         <span>y</span>
-      </h1>
+      </Logotype>
       <Dice />
-    </div>
+    </Wrapper>
   )
 };
 
