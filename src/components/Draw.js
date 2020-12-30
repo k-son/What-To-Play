67937492b050530e6f83cbd91d072c9de283bc07
Jacek Jarczok <kson.eu@gmail.com>
2020-12-30 10:29:05 +0100

    Beautify

--- src/components/Draw.js
+++ src/components/Draw.js
@@ -109,7 +109,11 @@ class Draw extends Component {
                 : null 
               }
               {songsLeft > 0 && 
-                <Link exact="true" to="/choice" className="Link-btn-choice" tabIndex="-1">
+                <Link 
+                  className="Link-btn-choice" 
+                  exact="true" 
+                  to="/choice" 
+                  tabIndex="-1">
                   <Button 
                     action="choose"
                     icon={<IconChoice />} 
