1206f50ea3ff0eb806cb8d00d842c5754848ffc1
Jacek Jarczok <kson.eu@gmail.com>
2021-09-03 14:26:43 +0200

    restyle

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -4,6 +4,8 @@ export const theme = {
     yellow: '#b19d2d',
     red: '#c25026',
     green: '#448735',
+    white: '#ffffff',
+    text: '#222222',
   },
   logoHeight: '54px',
   diceSide: '20px'
